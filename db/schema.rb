0633a80b4c0b6436537567bf0801c00d8dbf9a40
samiahl <sami.leo.ahl@gmail.com>
2015-04-17 17:56:13 +0300

    tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410223233) do
+ActiveRecord::Schema.define(version: 20150410220951) do
 
   create_table "references", force: :cascade do |t|
     t.string   "reference_type"
