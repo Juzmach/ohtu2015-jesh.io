f246d1e88abee5193767564f0489ff846b4f034f
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-04-24 19:22:47 +0300

    fixed db

--- db/migrate/20150410220951_create_references.rb
+++ db/migrate/20150410220951_create_references.rb
@@ -7,14 +7,6 @@ class CreateReferences < ActiveRecord::Migration
       t.string :year
       t.string :publisher
       t.string :volume
-      #optional
-      t.string :series
-      t.string :address
-      t.string :edition
-      t.string :month
-      t.string :note
-      t.string :key
-
       t.timestamps null: false
     end
   end
--- db/migrate/20150424145430_article_attributes.rb
+++ db/migrate/20150424145430_article_attributes.rb
@@ -1,11 +1,7 @@
 class ArticleAttributes < ActiveRecord::Migration
   def change
     add_column :references, :journal, :string
-    add_column :references, :volume, :string
-    #optional
     add_column :references, :number, :string
     add_column :references, :pages, :string
-
   end
 end
-#number, pages, month, note, key
